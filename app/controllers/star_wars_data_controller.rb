0c34a46b07745544cb098bb7c0fd480750df9a64
David Matthew Lange <david.m.lange@outlook.com>
2024-11-04 00:27:23 -0600

    Added controller functionality with API calls

--- app/controllers/star_wars_data_controller.rb
+++ app/controllers/star_wars_data_controller.rb
@@ -1,2 +1,16 @@
 class StarWarsDataController < ApplicationController
+  include HTTParty
+  base_uri "https://swapi.dev/api"
+
+  def people
+    @people = self.class.get("/people")["results"]
+  end
+
+  def planets
+    @planets = self.class.get("/planets")["results"]
+  end
+
+  def species
+    @species = self.class.get("/species")["results"]
+  end
 end
